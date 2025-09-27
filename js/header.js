361de98ee7127b273f110a871c3a412cd944fb3f
meguito <doasilva2009@hotmail.com>
2025-09-27 02:36:53 -0300

    Adição das revisões de texto iniciais

--- js/header.js
+++ js/header.js
@@ -5,10 +5,8 @@ window.addEventListener("scroll", () => {
   const currentScroll = window.pageYOffset;
 
   if (currentScroll > lastScroll) {
-    // rolando para baixo → esconder
     header.classList.add("hide");
   } else {
-    // rolando para cima → mostrar
     header.classList.remove("hide");
   }
 
